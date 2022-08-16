ff8b47ac9b314d6ea668e94904ba7adada7e3e75
jackline chepngeno <chepngenojackline0@gmail.com>
2022-08-16 21:27:20 +0300

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
 # Your code goes here!
+# define a function that takes an array of strings as input of argument arguments
+
+# Test case
+    # listen, %w[enlists google inlets banana] => [inlets]
+    # listen, %w[enlists google banana] => []
+    # listen, %w[] => []
+    # Shakespeare, [keshareapes, mango, ruby, james] => [keshareapes]
+
+
+    class Anagram
+        attr_accessor :word
+
+        def initialize(word)
+            @word = word
+        end
+        def match(array_names)
+            match_item = []
+            array_names.each do |item|
+                if @word.chars.sort == item.chars.sort
+                    match_item << item
+                end
+            end
+            match_item
+        end
+    end 
